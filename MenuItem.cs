d4d83b38dd9759e810d65446307b7cfa75f443ad
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2023-03-28 14:36:34 -0600

    creating menu browsing

---
+++ MenuItem.cs
@@ -0,0 +1,7 @@
+public class MenuItem {
+        public string? category { get; set; }
+        public int ID { get; set; }
+        public string? name { get; set; }
+        public int price { get; set; }
+        public string? image { get; set; }
+}
