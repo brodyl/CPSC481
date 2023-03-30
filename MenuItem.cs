f2fa95976e0fbf722fd11ca336f73c9ea6b94a28
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2023-03-29 22:14:13 -0600

    Fleshed out menu

--- MenuItem.cs
+++ MenuItem.cs
@@ -4,4 +4,5 @@ public class MenuItem {
         public string? name { get; set; }
         public int price { get; set; }
         public string? image { get; set; }
+        public string? tags { get; set; }
 }
