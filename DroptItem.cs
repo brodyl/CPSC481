280f717bc6658bf6c5abc89e107e1bea87bfa6e0
brodyl <75949899+brodyl@users.noreply.github.com>
2023-04-04 13:36:52 -0600

    Changes to the objects MenuItem, Ingredients, DropItems

--- DroptItem.cs
+++ DroptItem.cs
@@ -1,8 +1,6 @@
     public class DropItem
     {
-        public string Name { get; init; }
-        public decimal Price { get; init; }
         public int Quantity { get; set; }
         public string Customer { get; set; }
-        public string ImgPath { get; set; }
+        public MenuItem menuItem;
     }
