9c9de02894c2f6f1dc5f779f94b516dd71cdfe13
brodyl <75949899+brodyl@users.noreply.github.com>
2023-04-04 10:54:33 -0600

    StateContainer for Customize

---
+++ Ingredients.cs
@@ -0,0 +1,4 @@
+public class Ingredients {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+}
--- StateContainer.cs
+++ StateContainer.cs
@@ -42,8 +42,7 @@ public class StateContainer
     };
 
 
-
-
+    public MenuItem itemToCustomize;
 
     public event Action? OnChange;
 
