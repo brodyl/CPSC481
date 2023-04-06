89acdf00cf62813a3d522a780196c3f19e570be0
AzharFahim <fahimctgbd17@gmail.com>
2023-04-06 10:07:23 -0600

    update

--- StateContainer.cs
+++ StateContainer.cs
@@ -47,4 +47,27 @@ public class StateContainer
     public event Action? OnChange;
 
     private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+
+
+
+
+
+public class Item
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+
+    public decimal Total
+    {
+        get { return Quantity * Price; }
+    }
+}
+
+
+
+
+
 }
